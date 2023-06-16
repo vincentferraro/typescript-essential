aa8ca82c8b999c49b6d4264fde93e47e623e8729
Vincent Ferraro <v.ferraro@live.fr>
2023-06-16 09:24:43 +0200

    add Partial interface

--- src/models/userInterface.ts
+++ src/models/userInterface.ts
@@ -12,3 +12,66 @@ export interface refInterface<T> {
 export type refType<T> = {
   value: T;
 };
+
+// Union in interface
+export interface refUnion {
+  id?: number | string;
+  name: string;
+}
+
+const refUnValue: refUnion = {
+  id: "string",
+  name: "hello",
+};
+
+// interface Extension
+
+export interface YoungUser extends UserInterface {
+  isUnder18: Boolean;
+}
+
+const youngUser: YoungUser = {
+  id: 1,
+  name: "YounMen",
+  isUnder18: true,
+};
+
+// Intersect type
+
+type UserType = {
+  id?: string;
+  name: string;
+};
+
+type isUnder18 = {
+  isUnder18: Boolean;
+};
+
+type YoungMenType = UserType & isUnder18;
+
+const youngMenUser: YoungMenType = {
+  id: "2",
+  name: "YoungMenUser",
+  isUnder18: false,
+};
+
+// ReadOnly
+export type UserTypeWithReadOnly = Readonly<UserType>;
+
+const user: UserTypeWithReadOnly = {
+  name: "Helololo",
+};
+
+// use.name = "EH"
+
+// Partial
+
+function PartialFunction(user: UserType, userPartial: Partial<UserType>) {
+  return { ...user, ...userPartial };
+}
+
+const userStandard: UserType = {
+  id: "2",
+  name: "UserStandard",
+};
+const userComplete = PartialFunction(userStandard, { name: "Use" });
